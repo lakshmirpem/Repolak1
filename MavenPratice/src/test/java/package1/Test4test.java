a5e5c6d32b4fc7e978869ff4e2d792f132702079
lakshmirpem <lakshmirpem20@gmail.com>
2023-06-02 17:14:15 +0530

    commit 4

---
+++ MavenPratice/src/test/java/package1/Test4test.java
@@ -0,0 +1,15 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Test4test 
+{
+	@Test
+	public void m() 
+	{
+		System.out.println("Welcome to test");
+		System.out.println("Bye");
+		System.out.println("Hi");
+		
+	}
+}
