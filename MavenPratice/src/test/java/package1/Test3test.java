0ab1ba8396c1b186dc87407020ad90efde1592d7
lakshmirpem <lakshmirpem20@gmail.com>
2023-06-02 17:08:59 +0530

    commit

---
+++ MavenPratice/src/test/java/package1/Test3test.java
@@ -0,0 +1,15 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Test3test 
+{
+	@Test
+	public void m() 
+	{
+		System.out.println("Welcome to testyantra");
+		System.out.println("Bye Qspiders");
+		System.out.println("Hi jyothi");
+		
+	}
+}
